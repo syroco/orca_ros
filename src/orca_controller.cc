3d569b6ab3940080d95129c2a066badddd959ff8
Antoine Hoarau <hoarau.robotics@gmail.com>
2018-05-23 21:22:07 +0200

    add simple cmake

---
+++ src/orca_controller.cc
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <orca/orca.h>
+#include <sstream>
+
+int main(int argc, char const *argv[])
+{
+    ros::init(argc, argv, "orca_controller");
+    orca::Controller controller;
+    ros::NodeHandle nh;
+    
+    ros::Rate loop_rate(10);
+
+    unsigned long long count = 0;
+    while (ros::ok())
+    {
+
+      
+
+      ros::spinOnce();
+      loop_rate.sleep();
+      ++count;
+    }
+    return 0;
+}
