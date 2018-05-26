22f2591b0658c3a1a1eee35a20c074f56514adc7
Ryan Lober <rlober@gmail.com>
2018-05-26 11:33:47 +0200

    Gen constraint and proxy implemented

--- src/constraint/RosGenericConstraint.cc
+++ src/constraint/RosGenericConstraint.cc
@@ -8,10 +8,53 @@ RosGenericConstraint::RosGenericConstraint(const std::string& robot_name,
 : gc_(gen_con)
 , RosTaskBase(robot_name, controller_name, "constraints", gc_)
 {
-
+    getNodeHandle()->advertiseService( "print", &RosGenericConstraint::printService, this);
+    getNodeHandle()->advertiseService( "getSize", &RosGenericConstraint::getSizeService, this);
+    getNodeHandle()->advertiseService( "rows", &RosGenericConstraint::rowsService, this);
+    getNodeHandle()->advertiseService( "cols", &RosGenericConstraint::colsService, this);
+    getNodeHandle()->advertiseService( "getLowerBound", &RosGenericConstraint::getLowerBoundService, this);
+    getNodeHandle()->advertiseService( "getUpperBound", &RosGenericConstraint::getUpperBoundService, this);
+    getNodeHandle()->advertiseService( "getConstraintMatrix", &RosGenericConstraint::getConstraintMatrixService, this);
 }
 
 RosGenericConstraint::~RosGenericConstraint()
 {
 
+}
+
+bool RosGenericConstraint::printService(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
+{
+    gc_->print();
+    return true;
+}
+bool RosGenericConstraint::getSizeService(orca_ros::GetSize::Request& req, orca_ros::GetSize::Response& res)
+{
+    res.rows = gc_->getSize().rows();
+    res.cols = gc_->getSize().cols();
+    return true;
+}
+bool RosGenericConstraint::rowsService(orca_ros::GetInt::Request& req, orca_ros::GetInt::Response& res)
+{
+    res.value = gc_->rows();
+    return true;
+}
+bool RosGenericConstraint::colsService(orca_ros::GetInt::Request& req, orca_ros::GetInt::Response& res)
+{
+    res.value = gc_->cols();
+    return true;
+}
+bool RosGenericConstraint::getLowerBoundService(orca_ros::GetMatrix::Request& req, orca_ros::GetMatrix::Response& res)
+{
+    tf::matrixEigenToMsg(gc_->getLowerBound(), res.data);
+    return true;
+}
+bool RosGenericConstraint::getUpperBoundService(orca_ros::GetMatrix::Request& req, orca_ros::GetMatrix::Response& res)
+{
+    tf::matrixEigenToMsg(gc_->getUpperBound(), res.data);
+    return true;
+}
+bool RosGenericConstraint::getConstraintMatrixService(orca_ros::GetMatrix::Request& req, orca_ros::GetMatrix::Response& res)
+{
+    tf::matrixEigenToMsg(gc_->getConstraintMatrix(), res.data);
+    return true;
 }
---
+++ src/constraint/RosGenericConstraintProxy.cc
@@ -0,0 +1,91 @@
+#include <orca_ros/constraint/RosGenericConstraintProxy.h>
+
+using namespace orca_ros::constraint;
+
+RosGenericConstraintProxy::RosGenericConstraintProxy(   const std::string& robot_name,
+                                                        const std::string& controller_name,
+                                                        const std::string& constraint_name
+                                                        )
+: RosTaskBaseProxy(robot_name, controller_name, "constraints", constraint_name)
+{
+    sc_print_ = getNodeHandle()->serviceClient<std_srvs::Empty>("print");
+    sc_getSize_ = getNodeHandle()->serviceClient<orca_ros::GetSize>("getSize");
+    sc_rows_ = getNodeHandle()->serviceClient<orca_ros::GetInt>("rows");
+    sc_cols_ = getNodeHandle()->serviceClient<orca_ros::GetInt>("cols");
+    sc_getLowerBound_ = getNodeHandle()->serviceClient<orca_ros::GetMatrix>("getLowerBound");
+    sc_getUpperBound_ = getNodeHandle()->serviceClient<orca_ros::GetMatrix>("getUpperBound");
+    sc_getConstraintMatrix_ = getNodeHandle()->serviceClient<orca_ros::GetMatrix>("getConstraintMatrix");
+}
+
+RosGenericConstraintProxy::~RosGenericConstraintProxy()
+{
+}
+
+void RosGenericConstraintProxy::print()
+{
+    std_srvs::Empty srv;
+    if(!sc_print_.call(srv))
+    {
+        ROS_ERROR("Service call [print] failed.");
+    }
+}
+orca::math::Size RosGenericConstraintProxy::getSize()
+{
+    orca_ros::GetSize srv;
+    if(!sc_getSize_.call(srv))
+    {
+        ROS_ERROR("Service call [getSize] failed.");
+    }
+    return orca::math::Size(srv.response.rows, srv.response.cols);
+}
+int RosGenericConstraintProxy::rows()
+{
+    orca_ros::GetInt srv;
+    if(!sc_rows_.call(srv))
+    {
+        ROS_ERROR("Service call [rows] failed.");
+    }
+    return srv.response.value;
+}
+int RosGenericConstraintProxy::cols()
+{
+    orca_ros::GetInt srv;
+    if(!sc_cols_.call(srv))
+    {
+        ROS_ERROR("Service call [cols] failed.");
+    }
+    return srv.response.value;
+}
+Eigen::VectorXd RosGenericConstraintProxy::getLowerBound()
+{
+    orca_ros::GetMatrix srv;
+    if(!sc_getLowerBound_.call(srv))
+    {
+        ROS_ERROR("Service call [getLowerBound] failed.");
+    }
+    Eigen::MatrixXd m;
+    orca_ros::utils::floatMultiArrayToEigen(srv.response.data, m);
+    return m;
+}
+Eigen::VectorXd RosGenericConstraintProxy::getUpperBound()
+{
+    orca_ros::GetMatrix srv;
+    if(!sc_getUpperBound_.call(srv))
+    {
+        ROS_ERROR("Service call [getUpperBound] failed.");
+    }
+    Eigen::VectorXd m;
+    orca_ros::utils::floatMultiArrayToEigen(srv.response.data, m);
+    return m;
+}
+Eigen::MatrixXd RosGenericConstraintProxy::getConstraintMatrix()
+{
+    orca_ros::GetMatrix srv;
+    if(!sc_getConstraintMatrix_.call(srv))
+    {
+        ROS_ERROR("Service call [getConstraintMatrix] failed.");
+    }
+    Eigen::MatrixXd m;
+    orca_ros::utils::floatMultiArrayToEigen(srv.response.data, m);
+    return m;
+}
